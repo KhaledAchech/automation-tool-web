960307830f99dc4cb3bcc0833939641c54616be5
KhaledAchech <achechkhaled@gmail.com>
2022-03-10 17:07:32 +0100

    sidebar && header && footer

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { TenantsComponent } from './modules/tenants/tenants.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [{
+    path:'',
+    component: DashboardComponent
+  }, {
+    path:'tenants',
+    component: TenantsComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ front/src/app/layouts/default/default.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { TenantsComponent } from 'src/app/modules/tenants/tenants.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    TenantsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule
+  ]
+})
+export class DefaultModule { }
