23396222ed0addb47c813a14314bfc78bfd349ec
KhaledAchech <achechkhaled@gmail.com>
2022-07-26 18:31:48 +0200

    Fix new user image

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -39,7 +39,7 @@ import { MatIconModule } from '@angular/material/icon';
     AppRoutingModule,
     BrowserAnimationsModule,
     DefaultModule,
-     HttpClientModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     MatIconModule
